25c675ec890bf956acbe58f233aa21a4237be3bc
Karlo <49595438+Karl255@users.noreply.github.com>
2020-02-09 01:26:39 +0100

    Basic UI functionality

---
+++ BindableValue.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace YetAnotherMinesweeperClone
+{
+	public class BindableValue<T> : INotifyPropertyChanged
+	{
+		private T _value;
+		public T Value
+		{
+			get => _value;
+			set
+			{
+				_value = value;
+				OnPropertyChanged("Value");
+			}
+		}
+
+		public BindableValue() { }
+		public BindableValue(T value) => _value = value;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string info)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,49 @@ namespace YetAnotherMinesweeperClone
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public BindableValue<double> Scale = new BindableValue<double>(20);
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			Binding scaleBinding = new Binding("Value")
+			{
+				Source = Scale
+			};
+
+			for (int i = 0; i < 9; i++)
+			{
+				Minefield.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+			}
+
+			for (int i = 0; i < 9; i++)
+			{
+				Minefield.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+				for (int j = 0; j < 9; j++)
+				{
+					Image tile = new Image() 
+					{
+						Source = new BitmapImage(new Uri("Assets/Tile.png", UriKind.Relative))
+					};
+					tile.SetBinding(WidthProperty, scaleBinding);
+					tile.SetBinding(HeightProperty, scaleBinding);
+					Grid.SetRow(tile, i);
+					Grid.SetColumn(tile, j);
+					Minefield.Children.Add(tile);
+				}
+			}
+		}
+
+		private void Minefield_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			var point = e.GetPosition(Minefield);
+			MessageBox.Show($"{ point.X % Scale.Value }, { point.Y % Scale.Value }");
+		}
+
+		private void SmileButton_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
